d64a064ac2e9a710fed850d3edd33a230d5b2e07
João Vitor dos Santos <57101445+joaovitorsts@users.noreply.github.com>
2020-02-26 09:47:41 -0300

    Capítulo 3 em andamento

---
+++ curso_php/cap03/basico/ola.php
@@ -0,0 +1,13 @@
+<div class="titulo">Olá PHP</div>
+
+<h2>Exemplo A</h2>
+<?php 
+echo 'Olá <br>';// \n não funciona dentro de HTML
+echo 'Mundo';
+?>
+
+<h2>Exemplo B</h2>
+<?= "Outra forma de me 'expressar'!" ?><!-- Proibido usar echo nessa tag -->
+
+<br>
+
