38962e1a97a02e21ef784a905ddddb4b8b311374
João Vitor dos Santos <57101445+joaovitorsts@users.noreply.github.com>
2020-03-06 21:23:33 -0300

    Módulo 3 finalizado
    Exemplos básicos de integração entre PHP e HTML

---
+++ curso_php/cap03/basico/comentarios.php
@@ -0,0 +1,20 @@
+<div class="titulo">Comentários PHP</div>
+
+<?php
+echo "Estou no PHP";
+// echo "Estou no PHP";
+# echo "Estou no PHP";
+// ?>
+
+<p>Depois do primeiro bloco!</p>
+
+<?php
+/*
+    Linha 1 do comentário
+    Linha 2 do comentário
+    Linha 3 do comentário
+?> */
+?>
+
+<p>Depois do segundo bloco</p>
+
---
+++ curso_php/cap03/basico/desafio.php
@@ -0,0 +1,7 @@
+<div class="titulo">Desafio</div>
+
+<ul>
+    <li><?= 1 + 1; ?></li>
+    <li><?php print 8 ?></li>
+    <li><?php echo 8+8 ?></li>
+</ul>
